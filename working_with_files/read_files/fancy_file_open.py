b2548550c46c52c23d0d0c28b12057e10ee94737
tima <sikorskitimofei@gmail.com>
2023-12-02 11:53:07 +0100

    files writing revisited

--- working_with_files/read_files/fancy_file_open.py
+++ working_with_files/read_files/fancy_file_open.py
@@ -1,3 +1,8 @@
 with open("spider.txt", "r") as file:
     for line in file:
         print(line.strip())
+
+with open("spider.txt", "r") as file:
+    lines = file.readlines()
+    lines.sort()
+    print(lines)
---
+++ working_with_files/read_files/write_novel.py
@@ -0,0 +1,2 @@
+with open('novel_structure.txt', 'w') as file:
+    file.writelines(['exposition\n', 'rising action\n', 'climax\n', 'falling\n', 'action\n', 'resolution\n'])
